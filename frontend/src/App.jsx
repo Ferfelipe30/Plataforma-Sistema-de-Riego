3d1c8fbd26126bc4f7c049e2048472d97760c898
Juan Felipe Fernandez <ferjuanfelipe7@gmail.com>
2025-09-16 09:36:00 -0500

    Iniciaizams el proyecto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useDebugValue, useEffect, useState } from 'react'
+import axios from "axios"
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    axios.get("https://127.0.0.1:8000/api/data/")
+      .then(res => setMessage(res.data.message))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+        <h1>Fronted en React</h1>
+        <p>Mensaje desde el backend: {message}</p>
+      </div>
+    </>
+  )
+}
+
+export default App
